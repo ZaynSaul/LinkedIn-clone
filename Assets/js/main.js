930a5927c090a8f14c6a0ac33a22c6584afdb039
Saul Zayn <zaynsaul@gmail.com>
2022-08-15 11:08:19 +0000

    added profile section

---
+++ Assets/js/main.js
@@ -0,0 +1,19 @@
+function show() {
+  var action = document.getElementById("comment-box");
+
+  if (action.style.display == "none") {
+    action.style.display = "block";
+  } else {
+    action.style.display = "none";
+  }
+}
+
+function show1() {
+  var action = document.getElementById("comment-box-1");
+
+  if (action.style.display == "none") {
+    action.style.display = "block";
+  } else {
+    action.style.display = "none";
+  }
+}
